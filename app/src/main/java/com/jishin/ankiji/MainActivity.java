2cabc8e4a8f43fdfecc00439a385ca46ac980cf7
LanAnhNghiem <lananh.nghiem0502@gmail.com>
2017-10-24 00:50:15 +0700

    Initial commit

---
+++ app/src/main/java/com/jishin/ankiji/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jishin.ankiji;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

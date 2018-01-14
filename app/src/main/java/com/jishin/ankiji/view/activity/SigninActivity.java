86773ba927af6b4d81aa5ef860fd98399d5fcd84
SPlayer <splayer1295@gmail.com>
2018-01-14 11:47:58 +0700

    Facebook login with Firebase

--- app/src/main/java/com/jishin/ankiji/view/activity/SigninActivity.java
+++ app/src/main/java/com/jishin/ankiji/view/activity/SigninActivity.java
@@ -2,7 +2,9 @@ package com.jishin.ankiji.view.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -10,11 +12,27 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.login.LoginManager;
+import com.facebook.login.LoginResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.jishin.ankiji.R;
+
+import java.util.Arrays;
 
 public class SigninActivity extends AppCompatActivity {
 
-    
+
     private EditText edtUsername;
     private EditText edtPassword;
     private Button btnLogin;
@@ -22,9 +40,15 @@ public class SigninActivity extends AppCompatActivity {
     private ImageView imgGoogle;
     private TextView txtCreateAcount;
     private TextView txtForgotPass;
-    
-    public static final String TAG = SigninActivity.class.getSimpleName();
+    ImageView profilePictureView;
+    Button logoutButton;
+    TextView txtName, txtEmail;
+    CallbackManager mCallbackManager;
+    String email, name;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
+    public static final String TAG = SigninActivity.class.getSimpleName();
 
 
     @Override
@@ -32,6 +56,7 @@ public class SigninActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signin);
 
+        FirebaseApp.initializeApp(this);
         getControls();
         setEvents();
     }
@@ -44,36 +69,36 @@ public class SigninActivity extends AppCompatActivity {
         imgGoogle = (ImageView) findViewById(R.id.img_google);
         txtCreateAcount = (TextView) findViewById(R.id.tv_create_an_account);
         txtForgotPass = (TextView) findViewById(R.id.tv_forgot_password);
+        mCallbackManager = CallbackManager.Factory.create();
     }
 
 
     private void setEvents() {
-        
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(SigninActivity.this, MainActivity.class);
                 startActivity(intent);
-                
+
             }
         });
-        
-        
+
         imgFacebook.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(SigninActivity.this, "Thanh Hoai ne!", Toast.LENGTH_SHORT).show();
+                FacebookLogin();
             }
         });
-        
-        
+
+
         imgGoogle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Toast.makeText(SigninActivity.this, "Huu Duc ne!", Toast.LENGTH_SHORT).show();
             }
         });
-        
+
         txtCreateAcount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -81,8 +106,8 @@ public class SigninActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        
-        
+
+
         txtForgotPass.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -91,6 +116,89 @@ public class SigninActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onStart() {
+        //LoginManager.getInstance();
+        super.onStart();
+        if (mAuth != null) {
+            mAuth.addAuthStateListener(mAuthListener);
 
+            // Check if user is signed in (non-null) and update UI accordingly.
+            FirebaseUser currentUser = mAuth.getCurrentUser();
+            if (currentUser != null) {
+                Log.d(TAG, "onStart:currentUser");
+                logoutButton.setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+    private void FacebookLogin() {
+        LoginManager.getInstance().logInWithReadPermissions(SigninActivity.this, Arrays.asList("email", "public_profile"));
+
+        LoginManager.getInstance().registerCallback(mCallbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.d("Success", "Login success");
+                handleFacebookAccessToken(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.d(TAG, "facebook:onCancel");
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+
+                Log.d(TAG, "facebook:onError", error);
+            }
+        });
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged: signed_in " + user.getUid());
+                    //updateUI(user);
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+
+    private void handleFacebookAccessToken(AccessToken token) {
+        Log.d(TAG, "handleFacebookAccessToken:" + token);
+        AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Toast.makeText(SigninActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        // ...
+                    }
+                });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        mCallbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 }
